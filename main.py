fbf1a5ced31feb6ad5b46e712071b307f2b284d7
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-02-15 23:36:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def find_mismatch(text):
                 opening_brackets_stack.pop()
                 if not (len(text) - 1 == i):
                     continue
-                if (len(opening_brackets_stack) != 0):
+                if (len(opening_brackets_stack) not == 0):
                     return len(text)
             else:
                 return i+1
@@ -33,7 +33,7 @@ def find_mismatch(text):
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    if (mismatch != None):
+    if (mismatch not == None):
         print(mismatch)
     else:
         print("Success")
